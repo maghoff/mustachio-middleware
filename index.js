9ce0707065f24343781c4a5234015e12e26a9de1
Magnus Hoff <maghoff@gmail.com>
2016-07-01 15:14:53 +0200

    Automatically select partials resolved based on NODE_ENV

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const mustachio = require('mustachio');
 module.exports = function (config) {
 	config = config || {};
 
-	const partials = new mustachio.partials.FsPartials(config.root, config.suffixes);
+	const prod = process.env['NODE_ENV'] === 'production';
+
+	const partials =
+		config.partials ? config.partials :
+		(prod ?
+			new mustachio.partials.Fs(config.root, config.suffixes) :
+			new mustachio.partials.FsWatch(config.root, config.suffixes));
 
 	return function (req, res, next) {
 		res.mu = function (templateName, data) {
